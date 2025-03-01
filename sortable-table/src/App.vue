0a7444575e661ec60e750d3ba3cef91e8376e7fc
Jiangyue Wang <Jiangyue.Wang@nqaits.gov.au>
2025-03-01 13:18:48 +1100

    created sortable-table vue app + cleaned starter code and files

---
+++ sortable-table/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import SortableTable from './components/SortableTable.vue'
+</script>
+
+<template>
+    <SortableTable/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ sortable-table/src/components/SortableTable.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <p>table is here</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
